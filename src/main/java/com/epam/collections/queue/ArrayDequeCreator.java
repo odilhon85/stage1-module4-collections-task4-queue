e61dc0d398b48ed20522b69315be606fb01d7872
odilhon85 <i.o.r@inbox.ru>
2022-10-11 11:31:47 +0500

    1-attempt

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -6,5 +6,32 @@ import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+//        Two players have two Queue<Integer> of cards with the same even sizes(>=2). They want to combine all the cards together into an ArrayDeque<Integer> according to the following rule.
+//
+//        First, both take turns adding two cards to the ArrayDeque<Integer>. Then, with each move, the player:
+//
+//        takes the top card from the ArrayDeque<Integer> and adds this card to the end of his Queue<Integer>
+//        then puts two cards from the beginning of his Queue<Integer> into the ArrayDeque<Integer>.
+//        The moves go until the both players' Queue<Integer> of cards are not empty.
+//
+//        Implement the program which gets two Queue<Integer> and creates ArrayDeque<Integer> in accordance with the rules of the game.
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+
+        deque.add(firstQueue.remove());
+        deque.add(firstQueue.remove());
+        deque.add(secondQueue.remove());
+        deque.add(secondQueue.remove());
+
+        while (!firstQueue.isEmpty() && !secondQueue.isEmpty()){
+            firstQueue.add(deque.removeLast());
+            deque.add(firstQueue.remove());
+            deque.add(firstQueue.remove());
+
+            secondQueue.add(deque.removeLast());
+            deque.add(secondQueue.remove());
+            deque.add(secondQueue.remove());
+        }
+
+        return deque;
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,28 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List<Integer> dishList = new ArrayList<>();
+        if(numberOfDishes==0)return dishList;
+
+        for (int i = 1; i <= numberOfDishes; i++) {
+            dishList.add(i);
+        }
+        List<Integer> eatenDishList = new ArrayList<>(dishList);
+        List<Integer> resultDish = new ArrayList<>();
+
+        for (int i = everyDishNumberToEat-1; eatenDishList.size()!=0;) {
+            resultDish.add(eatenDishList.remove(i));
+            if(i+everyDishNumberToEat-1 > eatenDishList.size()-1){
+                i=(i+everyDishNumberToEat)-eatenDishList.size()-1;
+                if(eatenDishList.size()==1)i=0;
+            }else{
+                i+=everyDishNumberToEat-1;
+            }
+        }
+    return resultDish;
     }
 }
